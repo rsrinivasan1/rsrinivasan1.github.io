f7418e64b7072ae8ba200df0f54439c3f01dfaf9
Raghav Srinivasan <rsrinivasan2021@gmail.com>
2022-12-29 01:18:51 -0500

    new updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
+updateDimensions = () => {
+	let navbar = document.querySelector(".navbar");
+	let button = document.querySelector("#dropdown-button");
+	let navbarHeight =
+		button.offsetHeight == 0
+			? navbar.offsetHeight
+			: Math.min(navbar.offsetHeight, button.offsetHeight);
+	let root = document.querySelector(":root");
+	root.style.setProperty("--navbarHeight", navbarHeight + "px");
+	console.log(navbarHeight);
+	let vid1 = document.querySelector(".project1");
+	let vid2 = document.querySelector(".project2");
+	let vid3 = document.querySelector(".project3");
+	let vid1Width = vid1.offsetWidth;
+	let vid2Width = vid2.offsetWidth;
+	let vid3Width = vid3.offsetWidth;
+	root.style.setProperty("--vid1Width", vid1Width + "px");
+	root.style.setProperty("--vid2Width", vid2Width + "px");
+	root.style.setProperty("--vid3Width", vid3Width + "px");
+	console.log("width: " + vid2Width);
+	console.log("height: " + vid2.offsetHeight);
+};
+
 window.onload = () => {
+	updateDimensions();
 	let options = {
 		strings: ["Hi, my name is Raghav."],
 		typeSpeed: 40,
 		backSpeed: 40,
 		showCursor: false,
+		startDelay: 1000,
 		onComplete: () => {
 			setTimeout(() => {
 				let subtext = new Typed("#subtext", options2);
-			}, 1000);
+			}, 500);
 		},
 	};
 	let options2 = {
@@ -35,17 +60,17 @@ window.onload = () => {
 
 	for (const elem of menuOptionsArray) {
 		elem.addEventListener("click", () => {
-			currentMenuOption.classList.remove("text-yellow-300");
+			currentMenuOption.classList.remove("text-orange-300");
 			currentMenuOption.classList.add("text-gray-400");
-			currentMenuOption.classList.add("md:hover:text-white");
 			currentMenuOption.classList.add("hover:text-white");
+			currentMenuOption.classList.remove("hover:text-orange-300");
 
 			currentMenuOption = elem;
 
+			currentMenuOption.classList.add("text-orange-300");
 			currentMenuOption.classList.remove("text-gray-400");
-			currentMenuOption.classList.add("text-yellow-300");
-			currentMenuOption.classList.remove("md:hover:text-white");
 			currentMenuOption.classList.remove("hover:text-white");
+			currentMenuOption.classList.add("hover:text-orange-300");
 		});
 	}
 
@@ -57,28 +82,7 @@ window.onload = () => {
 		dropdownMenu.classList.toggle("hidden");
 	};
 
-	onresize = (event) => {
-		let navbar = document.querySelector(".navbar");
-		let button = document.querySelector("#dropdown-button");
-		let navbarHeight =
-			button.offsetHeight == 0
-				? navbar.offsetHeight
-				: Math.min(navbar.offsetHeight, button.offsetHeight);
-		let root = document.querySelector(":root");
-		root.style.setProperty("--navbarHeight", navbarHeight + "px");
-		console.log(navbarHeight);
-
-		// let aboutMeContainer = document.querySelector("#about-me-container");
-		// let aboutMeContainerHeight = aboutMeContainer.offsetHeight;
-		// root.style.setProperty(
-		// 	"--aboutMeContainerHeight",
-		// 	aboutMeContainerHeight + "px"
-		// );
-		// let aboutMeTextBox2 = document.querySelector("#about-me-text-box-2");
-		// let aboutMeTextBox2Height = aboutMeTextBox2.offsetHeight;
-		// root.style.setProperty(
-		// 	"--aboutMeTextBox2Height",
-		// 	aboutMeTextBox2Height + "px"
-		// );
+	onresize = () => {
+		updateDimensions();
 	};
 };
