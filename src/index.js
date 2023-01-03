82613648d5552d00de9ce2cea01a2cafc6ba407f
Raghav Srinivasan <rsrinivasan2021@gmail.com>
2023-01-02 19:47:29 -0500

    finished version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+let navbarHeight = 0;
+let scrollingTo = "";
+
 updateDimensions = () => {
 	let navbar = document.querySelector(".navbar");
 	let button = document.querySelector("#dropdown-button");
-	let navbarHeight =
+	navbarHeight =
 		button.offsetHeight == 0
 			? navbar.offsetHeight
 			: Math.min(navbar.offsetHeight, button.offsetHeight);
 	let root = document.querySelector(":root");
 	root.style.setProperty("--navbarHeight", navbarHeight + "px");
-	console.log(navbarHeight);
 
 	let vid1 = document.querySelector(".project1");
 	let vid2 = document.querySelector(".project2");
@@ -18,22 +20,24 @@ updateDimensions = () => {
 	root.style.setProperty("--vid1Width", vid1Width + "px");
 	root.style.setProperty("--vid2Width", vid2Width + "px");
 	root.style.setProperty("--vid3Width", vid3Width + "px");
-	console.log("width: " + vid2Width);
-	console.log("height: " + vid2.offsetHeight);
 };
 
 window.onload = () => {
+	let menuOptionsArray = document.getElementsByClassName("menu-option");
+	let currentMenuOption = menuOptionsArray[0];
+
 	updateDimensions();
+
 	let options = {
 		strings: ["Hi, my name is Raghav."],
 		typeSpeed: 40,
 		backSpeed: 40,
 		showCursor: false,
-		startDelay: 1000,
+		startDelay: 500,
 		onComplete: () => {
 			setTimeout(() => {
 				let subtext = new Typed("#subtext", options2);
-			}, 500);
+			}, 1000);
 		},
 	};
 	let options2 = {
@@ -56,28 +60,53 @@ window.onload = () => {
 	const dropdown = document.getElementById("dropdown-button");
 	const dropdownMenu = document.getElementById("navbar-default");
 
-	let menuOptionsArray = document.getElementsByClassName("menu-option");
-	let currentMenuOption = menuOptionsArray[0];
-
 	for (const elem of menuOptionsArray) {
 		elem.addEventListener("click", () => {
-			currentMenuOption.classList.remove("text-orange-300");
-			currentMenuOption.classList.add("text-gray-400");
-			currentMenuOption.classList.add("hover:text-white");
-			currentMenuOption.classList.remove("hover:text-orange-300");
-
-			currentMenuOption = elem;
-
-			currentMenuOption.classList.add("text-orange-300");
-			currentMenuOption.classList.remove("text-gray-400");
-			currentMenuOption.classList.remove("hover:text-white");
-			currentMenuOption.classList.add("hover:text-orange-300");
+			updateMenuOptions(elem);
+			scrollingTo = elem.getAttribute("href");
+			dropdownMenu.classList.add("hidden");
 		});
 	}
+
+	window.addEventListener("scroll", () => {
+		scrollUpdateOptions();
+	});
 
 	dropdown.addEventListener("click", () => {
 		dropdownToggle();
 	});
+
+	updateMenuOptions = (elem) => {
+		currentMenuOption.classList.remove("text-orange-300");
+		currentMenuOption.classList.add("text-gray-400");
+		currentMenuOption.classList.add("hover:text-white");
+		currentMenuOption.classList.remove("hover:text-orange-300");
+
+		currentMenuOption = elem;
+
+		currentMenuOption.classList.add("text-orange-300");
+		currentMenuOption.classList.remove("text-gray-400");
+		currentMenuOption.classList.remove("hover:text-white");
+		currentMenuOption.classList.add("hover:text-orange-300");
+	};
+
+	scrollUpdateOptions = () => {
+		for (const elem of menuOptionsArray) {
+			let section = document.getElementById(
+				elem.getAttribute("href").substring(1)
+			);
+			if (section) {
+				if (window.scrollY >= section.offsetTop - navbarHeight) {
+					updateMenuOptions(elem);
+				}
+			} else {
+				updateMenuOptions(menuOptionsArray[0]);
+			}
+		}
+		if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+			updateMenuOptions(menuOptionsArray[menuOptionsArray.length - 1]);
+		}
+	};
 
 	dropdownToggle = () => {
 		dropdownMenu.classList.toggle("hidden");
